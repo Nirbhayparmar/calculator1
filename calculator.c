68c0b94980e7eb238d75a496d5176a5bc67babe6
Nirbhay Parmar <62185337+Nirbhayparmar@users.noreply.github.com>
2020-03-16 00:19:22 +0530

    Update calculator.c
    add another operation to compare the two numbers
    &
    some UI changes

--- calculator.c
+++ calculator.c
@@ -4,14 +4,18 @@
 void main()
 {
 int a,b,c;
-printf("\nenter value of a:");
-scanf("%d",&a);
-printf("\nenter value of b:");
-scanf("%d",&b);
+printf("this is a calculator, created by Nirbhay Parmar.");
+printf("Menu:\n");
 printf("\nc=1:sum");
 printf("\nc=2:difference");
 printf("\nc=3:multiplication");
 printf("\nc=4:reminder(%)");
+print("\nc=5:who is greater");
+printf("///////////////////////////////");
+printf("\nenter value of a:");
+scanf("%d",&a);
+printf("\nenter value of b:");
+scanf("%d",&b);
 printf("\nenter value of c:");
 scanf("%d",&c);
 switch(c)
@@ -23,10 +27,16 @@ switch(c)
     case 3: printf("multiplication of a and b is: %d",a*b);
     break;
     case 4: printf("reminder when we divide a by b is:%d",a%b);
-    default :
+    break;
+    case 5: if(a>b)
+            printf("%d is greater than %d",a,b);
+            else printf("%d is greater than %d",b,a);
+    break;
+    default:
         printf("operator not valid");
 
 }
+    
 getch();
 
 
